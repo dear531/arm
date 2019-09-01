eefe1dee007b7844df07ab2eda31120edd6b70e7
LeoCheung <dear531@21cn.com>
2019-09-01 16:58:07 +0800

    mla rd, rm, rs, rn--rd:=((rm * rs) +rn)

--- 04mul.c
+++ 04mul.c
@@ -2,14 +2,24 @@
 
 int main(void)
 {
-	int a = 2, b = 3, c = 4;
-	fprintf(stdout, "a:%d b:%d c:%d\n", a, b, c);
+	int a = 2, b = 3, c = 4, d = 5;
+	fprintf(stdout, "a:%d b:%d c:%d, d:%d\n", a, b, c, d);
 	asm volatile (
 		"mul %[c], %[a], %[b]\n"
 		:[c]"=&r"(c)
 		:[a]"r"(a), [b]"r"(b)
 		:
 	);
-	fprintf(stdout, "a:%d b:%d c:%d\n", a, b, c);
+	fprintf(stdout, "a:%d b:%d c:%d, d:%d\n", a, b, c, d);
+	a = 2, b = 3, c = 4, d = 5;
+	asm volatile (
+		/* mla rd, rm, rs, rn--rd:=((rm * rs) +rn) */
+		/* d = ((a * b) + c)--d=((2 * 3) + 4)=10 */
+		"mla %[d], %[a], %[b], %[c]\n"
+		:[d]"=&r"(d)
+		:[a]"r"(a), [b]"r"(b), [c]"r"(c)
+		:
+	);
+	fprintf(stdout, "a:%d b:%d c:%d, d:%d\n", a, b, c, d);
 	return 0;
 }
