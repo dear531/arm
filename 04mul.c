f5971a282de6088c97e1d0c36f43cdac961f153d
LeoCheung <dear531@21cn.com>
2019-09-01 17:26:29 +0800

    mls d=((4 - (2 * 3))= -2) and CFLAGS +=-march=armv7-a

--- 04mul.c
+++ 04mul.c
@@ -2,24 +2,26 @@
 
 int main(void)
 {
-	int a = 2, b = 3, c = 4, d = 5;
-	fprintf(stdout, "a:%d b:%d c:%d, d:%d\n", a, b, c, d);
+	int a = 2, b = 3, c = 4, d = 5, e = 6;
+	fprintf(stdout, "a:%d b:%d c:%d, d:%d, e:%d\n", a, b, c, d, e);
 	asm volatile (
 		"mul %[c], %[a], %[b]\n"
 		:[c]"=&r"(c)
 		:[a]"r"(a), [b]"r"(b)
 		:
 	);
-	fprintf(stdout, "a:%d b:%d c:%d, d:%d\n", a, b, c, d);
+	fprintf(stdout, "a:%d b:%d c:%d, d:%d, e:%d\n", a, b, c, d, e);
 	a = 2, b = 3, c = 4, d = 5;
 	asm volatile (
 		/* mla rd, rm, rs, rn--rd:=((rm * rs) +rn) */
-		/* d = ((a * b) + c)--d=((2 * 3) + 4)=10 */
+		/* d = (c + (a * b))--d=(4 + (2 * 3))=10 */
 		"mla %[d], %[a], %[b], %[c]\n"
-		:[d]"=&r"(d)
+		/* d = ((c - a * b))--d=((4 - (2 * 3))= -2)*/
+		"mls %[e], %[a], %[b], %[c]\n"
+		:[d]"=&r"(d), [e]"=&r"(e)
 		:[a]"r"(a), [b]"r"(b), [c]"r"(c)
 		:
 	);
-	fprintf(stdout, "a:%d b:%d c:%d, d:%d\n", a, b, c, d);
+	fprintf(stdout, "a:%d b:%d c:%d, d:%d, e:%d\n", a, b, c, d, e);
 	return 0;
 }
--- Makefile
+++ Makefile
@@ -1,1 +1,2 @@
 CC	=arm-none-linux-gnueabi-gcc
+CFLAGS	+=-march=armv7-a
