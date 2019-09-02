e897ab6ab001719791a6b87b90ac37a4e06f6320
LeoCheung <dear531@21cn.com>
2019-09-02 22:57:37 +0800

    comments

--- 08tst.c
+++ 08tst.c
@@ -5,6 +5,7 @@ int main(void)
 	int a = 0xa0, b = 1;
 	fprintf(stdout, "a:%x, b:%x\n", a, b);
 	asm volatile (
+		/* 同时判断a变量绑定的寄存器的第4，5位是否同时为零 */
 		"tst %[a], #(0x03 << 4)\n"
 		"moveq %[b], #1\n"
 		"movne %[b], #0\n"
