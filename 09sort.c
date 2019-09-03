e893f1d0d850cb5639773244507ae741a4dde6d6
LeoCheung <dear531@21cn.com>
2019-09-03 22:42:38 +0800

    sort optimized

--- 09sort.c
+++ 09sort.c
@@ -2,19 +2,29 @@
 
 int main(void)
 {
-	int a = 1, b = 2;
-	fprintf(stdout, "a:%d, b:%d\n", a, b);
+	int a = 1, b = 2, c = 3;
+	fprintf(stdout, "a:%d, b:%d, c:%d\n", a, b, c);
 	asm volatile (
 		/* 排序 */
-		"mov r0, %[a]\n"
-		"mov r1, %[b]\n"
 		"cmp %[a], %[b]\n"
-		"movlt %[a], r1\n"
+		"movlt r0, %[a]\n"
+		"movlt %[a], %[b]\n"
 		"movlt %[b], r0\n"
-		:[a]"+&r"(a) ,[b]"+&r"(b)
+
+		"cmp %[a], %[c]\n"
+		"movlt r0, %[a]\n"
+		"movlt %[a], %[c]\n"
+		"movlt %[c], r0\n"
+
+		"cmp %[b], %[c]\n"
+		"movlt r0, %[b]\n"
+		"movlt %[b], %[c]\n"
+		"movlt %[c], r0\n"
+
+		:[a]"+&r"(a) ,[b]"+&r"(b), [c]"+&r"(c)
 		:
-		:"r0", "r1"
+		:"r0"
 	);
-	fprintf(stdout, "a:%d, b:%d\n", a, b);
+	fprintf(stdout, "a:%d, b:%d, c:%d\n", a, b, c);
 	return 0;
 }
