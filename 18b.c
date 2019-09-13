116989f4a542b1a7b6a53cb9de1b56070901f416
LeoCheung <dear531@21cn.com>
2019-09-13 10:38:12 +0800

    a + b + c + d + e + f for asm call for APCS

--- 18b.c
+++ 18b.c
@@ -9,7 +9,7 @@ int main(void)
 	 * 2.满递减栈 fd
 	 * 3.返回值，32位r0，64位放r0r1，r0低32位，r1高32位
 	 */
-	int a = 1, b = 2, c = 0;
+	int a = 1, b = 2, c = 0, d = 0, e = 0, f = 0, g = 0;
 	fprintf(stdout, "asm add1 before a:%d, b:%d, c:%d\n",
 		a, b, c);
 	__asm__ __volatile__(
@@ -39,7 +39,44 @@ int main(void)
 	);
 	fprintf(stdout, "asm add2 after a:%d, b:%d, c:%d\n",
 		a, b, c);
+	a = 1;
+	b = 2;
+	c = 3;
+	d = 4;
+	e = 5;
+	f = 6;
+	g = 0;
+	fprintf(stdout,
+		"add3 before a:%d, b:%d, c:%d, d:%d, e:%d, f:%d, g:%d\n",
+		a, b, c, d, e, f, g);
+	__asm__ __volatile__(
+		"ldr r0, %[e]\n"
+		"ldr r1, %[f]\n"
+		"push {r0, r1}\n"
+		"ldr r0, %[a]\n"
+		"ldr r1, %[b]\n"
+		"ldr r2, %[c]\n"
+		"ldr r3, %[d]\n"
+		/* bl是跳转之前将下条指令的地址压栈 */
+		"bl add3\n"
+		"mov %[g], r0\n"
+		"add sp, sp, #8\n"
+		:[g]"=&r"(g)
+		:[a]"m"(a), [b]"m"(b), [c]"m"(c), [d]"m"(d), [e]"m"(e), [f]"m"(f) 
+		:
+	);
+	fprintf(stdout,
+		"add3 after a:%d, b:%d, c:%d, d:%d, e:%d, f:%d, g:%d\n",
+		a, b, c, d, e, f, g);
 	return 0;
+}
+
+int add3(int a, int b, int c ,int d, int e, int f)
+{
+	fprintf(stdout,
+		"in the add3 a:%d, b:%d, c:%d, d:%d, e:%d, f:%d\n",
+		a, b, c, d, e, f);
+	return a + b + c + d + e + f;
 }
 int add2(int a, int b)
 {
