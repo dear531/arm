47b47bb31d55919775f29f72cff09ad832385b95
LeoCheung <dear531@21cn.com>
2019-11-16 13:32:32 +0800

    small page table for mmu

--- hardwere/11mmu/src/main.c
+++ hardwere/11mmu/src/main.c
@@ -6,9 +6,15 @@ int main(void)
 {
 	int *p = (void *)0x52345678;
 	*p = 100;
-	printf("*(%#010x)=%d\n", p, *p);
+#if 0
+	/* section table */
 	section_create_table(0x55000000);
 	section_table_mmap(0x12345678, 0x52345678);
+#else
+	/* small page table */
+	page_create_table(0x55000000, 0x55100000);
+	page_table_mmap(0x12345678, 0x52345678);
+#endif
 	mmu_enable();
 	printf("*(0x12345678)=%d, *(0x52345678)=%d\n",
 		*(unsigned int *)0x12345678, *(unsigned int *)0x52345678);
