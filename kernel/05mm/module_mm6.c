9a9f10cc4cafad0ac777b312c53564d643a1b4f4
LeoCheung <dear531@21cn.com>
2020-02-07 22:28:21 +0800

    cat /proc/slabinfo

--- kernel/05mm/module_mm6.c
+++ kernel/05mm/module_mm6.c
@@ -19,6 +19,9 @@ void ctor(void *data)
 	return;
 }
 
+static struct kmem_cache *kmem = NULL;
+static void *nrf = NULL;
+
 static __init int mm_init(void)
 {
 #if 0
@@ -26,8 +29,6 @@ struct kmem_cache *
 kmem_cache_create(const char *name, size_t size, size_t align,
 		  unsigned long flags, void (*ctor)(void *))
 #endif
-	struct kmem_cache *kmem = NULL;
-	void *nrf = NULL;
 	/* 在分配缓存当中每一个结构体时调用，会把结构体指针传给函数 */
 	kmem = kmem_cache_create("nrt_st", sizeof(struct nrf_st), 0, SLAB_HWCACHE_ALIGN, ctor);
 	printk("kmem:%p\n", kmem);
@@ -36,16 +37,18 @@ void *kmem_cache_alloc(struct kmem_cache *cachep, gfp_t flags)
 #endif
 	nrf = kmem_cache_alloc(kmem, GFP_KERNEL);
 	printk("nrf:%p\n", nrf);
-#if 0
-void kmem_cache_free(struct kmem_cache *cachep, void *objp)
-#endif
-	kmem_cache_free(kmem, nrf);
-	kmem_cache_destroy(kmem);
 	return 0;
 }
 
 static __exit void mm_exit(void)
 {
+#if 0
+void kmem_cache_free(struct kmem_cache *cachep, void *objp)
+#endif
+	kmem_cache_free(kmem, nrf);
+	nrf = NULL;
+	kmem_cache_destroy(kmem);
+	kmem = NULL;
 }
 
 module_init(mm_init);
